afe5b9b4da7ffb65bf0149845e78f757c35179bb
Ben Mathes <mathes.ben@gmail.com>
2011-12-23 03:07:38 -0800

    environment set up, getting the parsing/printing to work

--- puzzle.rb
+++ puzzle.rb
@@ -1,48 +1,106 @@
-# I decied to write this with a class mostly to play
-# with Ruby's class syntax/behavior. A quick script could easily have done the
-# job as well. Some of my comments may be overly verbose and explain
-# things obvious to someone who has spent a lot of time in Ruby;
-# As I am not an expert in Ruby, what is obvious and what needs documentation
-# to an export is as of yet unknown to me.
+=begin
+    I decied to write this with a class mostly to play
+    with Ruby's class syntax/behavior. A quick script could easily have done the
+    job well, especially since the puzzle is so specialized. Some of my comments 
+    may be overly verbose and explain things obvious to someone who has spent a 
+    lot of time in Ruby; As I am not an expert in Ruby, what is obvious and what
+     needs documentation to a ruby export is as of yet unknown to me.
+=end
 
-# HTML parsers. required for proper installation
-# sudo gem install nokogiri
-# sudo gem install open-uri
-require 'nokogiri'
+# HTML parser gems
 require 'open-uri'
+require 'nokogiri'
 
-class BlogGetter
 
-    # the shopittome blog URL doesn't change from instance-to-isntance
-    @@blogAddress = 'blog.shopittome.com'
+# this class will parse a blog and print the posts of
+# the shopittome blog
+class ShopItToMeBlogParser
+
 
     # the content might change, though, so it's an instance-level variable.
+    @blogURL
     @doc
+    @posts = []
 
-    # whether to use wget or 'curl -0' depends on the OS
-    @os = ''
 
-    # start up this instance by setting the OS we're on
-    def initialize(os)
-        @os = os.downcase
+    # initializer method that sets the URL of the blog we'll parse
+    def initialize(blogURL)
+        @blogURL = blogURL
     end
 
-    # fetches the latest source from @@blogAddress
-    # and parses it
-    def getLatestPost
-        @doc = Nokogiri::HTML(open(@@blogAddress))
-        
+
+    # fetches the latest source from the blog URL and parses it
+    def getHTML
+        @doc = Nokogiri::HTML(open(@blogURL))
     end
-    
-    def sayHi
-        puts "why, hello there!"
+
+
+    # parses the fetched @doc
+    def parsePosts
+
+        # this relies on the DOM structure of blog.shopittome.com:
+        #     <h2 class="title">...</h2>
+        #     <p class="post-body">...</p>
+        #     <h2 class="different-title">...</h2>
+        #     <p class="different-post-body">...</p>
+        # this means we have to iterate over everything that is at that flat level.
+        blogContent = @doc.css('#content_inner')
+        allPostsElements = blogContent.element_children()
+        thisPost = nil
+        allPostsElements.each do |element|
+            puts element.node_type()
+=begin
+            if thisElement.node_type() == ''
+                @posts.push({
+                    "title"  => title.content,
+                    "images" => []
+                })
+            end
+
+            if image
+                push image info on to this blog post  images
+            end
+
+            images = 
+            @posts.title
+=end
+        end
     end
+
+
+    # after posts are parsed and put into @posts, this prints them
+    def printPosts()
+        # go throuch each post...
+        i = 0
+        until i >= @posts.length 
+            post = @post[i]
+
+            # ... print the title...
+            puts "#{i}. #{post["title"]}"
+
+            # ... and all the images therein
+            post["images"].each do |image|
+                puts "  - #{image["src"]}"
+            end
+        end
+    end
+
+
+    # the "do everything" function, which is a convenience
+    def parseBlogAndPrint()
+        self.getHTML()
+        self.parsePosts()
+        self.printPosts()
+    end
+
+
 end
+
 
 
 
 ######### actual exection ######### 
 if __FILE__ == $0
-    blogGeter = BlogGetter.new
-    blogGeter.getLatestPost
+    blogParser = ShopItToMeBlogParser.new('http://blog.shopittome.com')
+    blogParser.parseBlogAndPrint()
 end
